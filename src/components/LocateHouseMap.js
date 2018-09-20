938e88152fc81a9ec193509d9437f2a20c14a762
Moisés Canett <moises.canett@gmail.com>
2018-09-20 16:53:45 -0700

    Added searchBox to LocateHouseMap

--- src/components/LocateHouseMap.js
+++ src/components/LocateHouseMap.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { compose, withProps } from 'recompose';
+import _ from 'lodash';
+import { compose, withProps, lifecycle } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
 
 const LocateHouseMap = compose(
   withProps({
@@ -9,6 +11,57 @@ const LocateHouseMap = compose(
     containerElement: <div style={{ height: `800px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {}
+
+      this.setState({
+        bounds: null,
+        zoom: 13,
+        center: {
+          lat: 32.6137537, lng: -115.4853104
+        },
+        markers: [],
+        onMapMounted: ref => {
+          refs.map = ref;
+        },
+        onBoundsChanged: () => {
+          this.setState({
+            bounds: refs.map.getBounds(),
+            center: refs.map.getCenter(),
+          })
+        },
+        onSearchBoxMounted: ref => {
+          refs.searchBox = ref;
+        },
+        onPlacesChanged: () => {
+          const places = refs.searchBox.getPlaces();
+          console.log(places);
+          const bounds = new google.maps.LatLngBounds();
+
+          places.forEach(place => {
+            if (place.geometry.viewport) {
+              bounds.union(place.geometry.viewport)
+            } else {
+              bounds.extend(place.geometry.location)
+            }
+          });
+          const nextMarkers = places.map(place => ({
+            position: place.geometry.location,
+          }));
+          const nextZoom = 16;
+          const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+          this.setState({
+            center: nextCenter,
+            zoom: nextZoom,
+            markers: nextMarkers,
+          });
+          // refs.map.fitBounds(bounds);
+        },
+      })
+    },
+  }),
   withScriptjs,
   withGoogleMap
   )((props) => {
@@ -16,10 +69,33 @@ const LocateHouseMap = compose(
   return (
     <GoogleMap
       onClick={props.onMapClick}
-      defaultZoom={13}
-      defaultCenter={{ lat: 32.6137537, lng: -115.4853104 }}
-      options={{streetViewControl: false, mapTypeControl: false}}
-    >
+      zoom={props.zoom}
+      center={props.center}
+      options={{ mapTypeControl: false }}
+      onBoundsChanged={props.onBoundsChanged}
+      >
+      {/*defaultCenter={{ lat: 32.6137537, lng: -115.4853104 }}*/}
+      <SearchBox
+        ref={props.onSearchBoxMounted}
+        bounds={props.bounds}
+        controlPosition={google.maps.ControlPosition.TOP_LEFT}
+        onPlacesChanged={props.onPlacesChanged}
+      >
+        <input
+          type="text"
+          placeholder="Busca la dirección"
+          className="text-input"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            marginTop: `27px`,
+            width: `340px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            textOverflow: `ellipses`,
+          }}
+        />
+      </SearchBox>
       {props.isMarkerShown && 
         <Marker 
           position={{ lat: props.markerPosition.lat, lng: props.markerPosition.lng }} 
